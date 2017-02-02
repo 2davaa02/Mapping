454315bb9d7c6f856a94d0273b10f09a8d8dbbed
2davaa02 <2davaa02@solent.ac.uk>
2017-02-02 12:59:31 +0000

    Search by coordinates

--- app/src/main/java/com/example/a2davaa02/mapping/HelloMap.java
+++ app/src/main/java/com/example/a2davaa02/mapping/HelloMap.java
@@ -3,15 +3,20 @@ package com.example.a2davaa02.mapping;
 import android.app.Activity;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import org.osmdroid.views.MapView;
-import org.osmdroid.util.GeoPoint;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import org.osmdroid.config.Configuration;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
 
-public class HelloMap extends Activity
+public class HelloMap extends Activity implements View.OnClickListener
+
 {
 
     MapView mv;
 
+    @Override
     public void onCreate(Bundle savedInstanceState)
     {
 
@@ -28,5 +33,29 @@ public class HelloMap extends Activity
         mv.setBuiltInZoomControls(true);
         mv.getController().setZoom(14);
         mv.getController().setCenter(new GeoPoint(40.1,22.5));
+
+        Button btn   = (Button)findViewById(R.id.searchLocation);
+        btn.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        EditText lat = (EditText)findViewById(R.id.latitude);
+        EditText lon = (EditText)findViewById(R.id.longitude);
+
+        if(lat.getText().toString().equals("")||lon.getText().toString().equals(""))
+        {
+            mv.getController().setZoom(14);
+            mv.getController().setCenter(new GeoPoint(0.0,0.0));
+        }
+        else
+        {
+            Float la = Float.parseFloat(lat.getText().toString());
+            Float lo = Float.parseFloat(lon.getText().toString());
+
+            mv.getController().setZoom(14);
+            mv.getController().setCenter(new GeoPoint(la, lo));
+        }
     }
 }
